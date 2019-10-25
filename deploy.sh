eb6a78ff0642b35cfb4ccc638db26cb5cc7a2d3b
Ashan Bakmeedeniya <ashan.bakmeedeniya@ifsworld.com>
2019-10-25 19:22:07 +0530

    update deploy file

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
-docker build -t ashanb/multi-client:latest -t ashanb/multi-client:$SHA -f /client/Dockerfile ./client
-docker build -t ashanb/multi-server:latest -t ashanb/multi-server:$SHA -f /server/Dockerfile ./server
-docker build -t ashanb/multi-worker:latest -t ashanb/multi-worker:$SHA -f /worker/Dockerfile ./worker
+docker build -t ashanb/multi-client:latest -t ashanb/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t ashanb/multi-server:latest -t ashanb/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t ashanb/multi-worker:latest -t ashanb/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
 docker push ashanb/multi-client:latest
 docker push ashanb/multi-server:latest
