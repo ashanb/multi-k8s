18e4a6b1a0de110b6835ff2c64729e0420a98ff5
Ashan Bakmeedeniya <ashan.bakmeedeniya@ifsworld.com>
2019-10-25 17:16:15 +0530

    updating cicd files

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+docker build -t ashanb/multi-client:latest -t ashanb/multi-client:$SHA -f /client/Dockerfile ./client
+docker build -t ashanb/multi-server:latest -t ashanb/multi-server:$SHA -f /server/Dockerfile ./server
+docker build -t ashanb/multi-worker:latest -t ashanb/multi-worker:$SHA -f /worker/Dockerfile ./worker
+docker push ashanb/multi-client:latest
+docker push ashanb/multi-server:latest
+docker push ashanb/multi-worker:latest
+docker push ashanb/multi-client:$SHA
+docker push ashanb/multi-server:$SHA
+docker push ashanb/multi-worker:$SHA
+kubectl apply -f k8s
+kubectl set image deployments/server-deployement server=ashanb/multi-server:$SHA
+
+kubectl set image deployments/client-deployement client=ashanb/multi-client:$SHA
+
+kubectl set image deployments/worker-deployement worker=ashanb/mulit-worker:$SHA
